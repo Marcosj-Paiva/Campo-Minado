fa9db12e5195aa2d9428f2070cda949e18abb121
Marcosj-Paiva <107214258+Marcosj-Paiva@users.noreply.github.com>
2025-05-26 22:57:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,175 @@
+//FUNÇÕES DA BOMBA
+
+function bomba(){
+  var block = document.getElementById("element");
+  block.classList.remove("block");
+  block.classList.add("bomba");
+
+  var abs= document.getElementById("loser");
+  abs.classList.remove("end");
+  abs.classList.add("end2");
+}
+
+function bomba2(){
+  var block = document.getElementById("element2");
+  block.classList.remove("block");
+  block.classList.add("bomba");
+
+  var abs= document.getElementById("loser");
+  abs.classList.remove("end");
+  abs.classList.add("end2");
+}
+
+function bomba3(){
+  var block = document.getElementById("element3");
+  block.classList.remove("block");
+  block.classList.add("bomba");
+
+  var abs= document.getElementById("loser");
+  abs.classList.remove("end");
+  abs.classList.add("end2");
+}
+
+function bomba4(){
+  var block = document.getElementById("element4");
+  block.classList.remove("block");
+  block.classList.add("bomba");
+
+  var abs= document.getElementById("loser");
+  abs.classList.remove("end");
+  abs.classList.add("end2");
+}
+
+//FUNÇÔES PARA O ESPAÇO VAZIO
+
+function vazio(){
+  var block = document.getElementById("vazio");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio2(){
+  var block = document.getElementById("vazio2");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio3(){
+  var block = document.getElementById("vazio3");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio4(){
+  var block = document.getElementById("vazio4");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio5(){
+  var block = document.getElementById("vazio5");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio6(){
+  var block = document.getElementById("vazio6");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio7(){
+  var block = document.getElementById("vazio7");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio8(){
+  var block = document.getElementById("vazio8");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio9(){
+  var block = document.getElementById("vazio9");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio10(){
+  var block = document.getElementById("vazio10");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio11(){
+  var block = document.getElementById("vazio11");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio12(){
+  var block = document.getElementById("vazio12");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio13(){
+  var block = document.getElementById("vazio13");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio14(){
+  var block = document.getElementById("vazio14");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio15(){
+  var block = document.getElementById("vazio15");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio16(){
+  var block = document.getElementById("vazio16");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio17(){
+  var block = document.getElementById("vazio17");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio18(){
+  var block = document.getElementById("vazio18");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio19(){
+  var block = document.getElementById("vazio19");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio20(){
+  var block = document.getElementById("vazio20");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+function vazio21(){
+  var block = document.getElementById("vazio21");
+  block.classList.remove("block");
+  block.classList.add("vazio")
+}
+
+//FUNÇÃO PARA RECARREGAR PAGINA
+
+function refresh(){
+  document.location.reload(true);
+}
